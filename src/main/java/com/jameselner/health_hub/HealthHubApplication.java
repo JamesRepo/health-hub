94d865842d6ab9122517437d5a41197e9b07e759
James Elner <elner.k.james@gmail.com>
2026-01-06 20:42:22 +0000

    first commit

---
+++ src/main/java/com/jameselner/health_hub/HealthHubApplication.java
@@ -0,0 +1,13 @@
+package com.jameselner.health_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthHubApplication.class, args);
+	}
+
+}
